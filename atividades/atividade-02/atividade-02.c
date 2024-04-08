f3ed81836d2107950d1956de0040fb32dbb287a0
Italo Aguiar <164262891+italo-aguiar@users.noreply.github.com>
2024-04-08 19:02:50 -0300

    Update atividade-02.c

--- atividades/atividade-02/atividade-02.c
+++ atividades/atividade-02/atividade-02.c
@@ -1,8 +1,10 @@
+//Impressão de números pares
+
 #include <stdio.h> 
 int main() { 
   int numero;  
   for (numero = 0; numero <= 50; numero++) { 
-    if (numero % 2 == 0) { 
+    if ((numero % 2 == 0)  & (numero!=0)) { 
       printf("%d\n", numero); 
     } else {
     printf("\n");
