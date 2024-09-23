1228e43df24f8b7e2ea714dc0661ad816f3c6107
Italo Aguiar <164262891+italo-aguiar@users.noreply.github.com>
2024-09-23 19:28:13 -0300

    Update avaliacao-01.c

--- avaliacoes/avaliacao-01/avaliacao-01.c
+++ avaliacoes/avaliacao-01/avaliacao-01.c
@@ -1,5 +1,27 @@
 /*
-  Criar um repositório no GitHub com a estrtura mostrada na aula de 27-03-2024 e que está documentado em Regras.
+  Criar um repositório no GitHub com a estrtura mostrada na aula de 27-03-2024¹ e que está documentado em Regras.
 Evidenciar no Google Classrom com o link da atividade.
 */
 
+
+// ¹Estrutura:
+
+/*
+A seguinte estrutura de pastas deverá ser seguida no repositório:
+
+<nome-do-repositório>
+                                      |
+                                      |--<avaliacoes>
+                                      |                       |
+                                      |                       |--<avaliacao-01>
+                                      |                       |                           |-avaliacao-01.pdf
+                                      |                       |                           |-avaliacao-01.c
+                                      |                       |-<avaliacao-02>
+                                      |                       |                           |-avaliacao-02.pdf
+                                      |                       |                           |-avaliacao-02.c 
+                                      |--<atividades>
+                                      |                       |-<atividade-01>
+                                      |                       |                         |-atividade01.pdf
+                                      |                       |-<atividade-02>
+                                      |                       |                          |-atividade-02.c
+  */
