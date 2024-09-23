c35d89114f1f9a27561da21582852fb08544151a
Italo Aguiar <164262891+italo-aguiar@users.noreply.github.com>
2024-09-23 19:30:11 -0300

    Create avaliacao.c

---
+++ avaliacoes/avaliacao-03/avaliacao.c
@@ -0,0 +1,7 @@
+/*
+Postar os exercícios 01 a 08 no Github em uma pasta Atividade-03 mostrando o código e as saídas. 
+
+A entrega feita no Google Ckassrom deve ser o link da Atividade-03 do GitHub.
+*/
+
+//Link: https://github.com/italo-aguiar/LLP_24.1/tree/main/atividades
